dff0ea9bbd37825c20ecef9f3504084822b4d644
Jordan Dean <100329177+JDean2814@users.noreply.github.com>
2022-03-21 13:44:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ let userInput = document.getElementById('list-input');
 let groceryList = document.getElementById('grocery-list');
 let errorMessage = document.getElementById('error-message');
 
+document.addEventListener('DOMContentLoaded', loadLocalStorage);
 
 addItemButton.onclick = function() {
-    let itemContain = document.createElement('div');
-    let item = document.createElement('p');
-    let buttonDiv = document.createElement('div');
-    let deleteButton = document.createElement('button');
-    let checkButton = document.createElement('button');
-
     if (userInput.value === "") {
         userInput.style.outline = '2px solid red';
         addItemButton.style.borderLeft = '3px solid red'
         window.alert("Please enter item into the input.")
     } else {
+        let itemContain = document.createElement('div');
+        let item = document.createElement('p');
+        let buttonDiv = document.createElement('div');
+        let deleteButton = document.createElement('button');
+        let checkButton = document.createElement('button');
+
         itemContain.classList.add('item-container');
         deleteButton.classList.add('delete-button');
         checkButton.classList.add('check-button');
         item.innerText = userInput.value;
+        saveLocalStorage(userInput.value);
         checkButton.innerText = '-'
         deleteButton.innerText = 'X';
         groceryList.appendChild(itemContain);
@@ -30,6 +32,8 @@ addItemButton.onclick = function() {
         userInput.value = "";
             
         deleteButton.onclick = function() {
+            const groceryItem = item.innerText;
+            removeGroceryItem(groceryItem);
             groceryList.removeChild(itemContain);
         }
             
@@ -40,4 +44,65 @@ addItemButton.onclick = function() {
         userInput.style.outline = '';
         addItemButton.style.borderLeft = ''
     }
+}
+
+function saveLocalStorage(item) {
+    let groceryItems;
+    if(localStorage.getItem('groceryItems') === null) {
+        groceryItems = [];
+    } else {
+        groceryItems = JSON.parse(localStorage.getItem('groceryItems'));
+    }
+    groceryItems.push(item);
+    localStorage.setItem('groceryItems', JSON.stringify(groceryItems))
+}
+
+function loadLocalStorage() {
+    let groceryItems;
+    if(localStorage.getItem('groceryItems') === null) {
+        groceryItems = [];
+    } else {
+        groceryItems = JSON.parse(localStorage.getItem('groceryItems'));
+    }
+    groceryItems.forEach(function(groceryItem) { 
+        let itemContain = document.createElement('div');
+        let item = document.createElement('p');
+        let buttonDiv = document.createElement('div');
+        let deleteButton = document.createElement('button');
+        let checkButton = document.createElement('button');
+
+        itemContain.classList.add('item-container');
+        deleteButton.classList.add('delete-button');
+        checkButton.classList.add('check-button');
+        item.innerText = groceryItem;
+        checkButton.innerText = '-'
+        deleteButton.innerText = 'X';
+        groceryList.appendChild(itemContain);
+        itemContain.appendChild(item);
+        itemContain.appendChild(buttonDiv);
+        buttonDiv.appendChild(checkButton);
+        buttonDiv.appendChild(deleteButton);
+
+        deleteButton.onclick = function() {
+            const groceryItem = item.innerText;
+            removeGroceryItem(groceryItem);
+            groceryList.removeChild(itemContain);
+        }
+            
+        checkButton.onclick = function() {
+            item.classList.toggle('line');
+        }
+    });
+}
+
+function removeGroceryItem(groceryItem) {
+    let groceryItems;
+    if(localStorage.getItem('groceryItems') === null) {
+        groceryItems = [];
+    } else {
+        groceryItems = JSON.parse(localStorage.getItem('groceryItems'));
+    }
+    const index = groceryItems.indexOf(groceryItem.toLowerCase(), 0);
+    groceryItems.splice(index, 1);
+    localStorage.setItem("groceryItems", JSON.stringify(groceryItems));
 }

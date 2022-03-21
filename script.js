0f95790170ede86550cebacd8cb987008910a4f9
Jordan Dean <100329177+JDean2814@users.noreply.github.com>
2022-03-21 11:16:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 let addItemButton = document.getElementById('add-button');
 let userInput = document.getElementById('list-input');
 let groceryList = document.getElementById('grocery-list');
+let errorMessage = document.getElementById('error-message');
+
 
 addItemButton.onclick = function() {
     let itemContain = document.createElement('div');
     let item = document.createElement('p');
+    let buttonDiv = document.createElement('div');
     let deleteButton = document.createElement('button');
-    itemContain.classList.add('item-container');
-    item.innerText = userInput.value;
-    deleteButton.innerText = 'X';
-    groceryList.appendChild(itemContain);
-    itemContain.appendChild(item);
-    itemContain.appendChild(deleteButton);
-    userInput.value = "";
+    let checkButton = document.createElement('button');
 
-    deleteButton.onclick = function() {
-        groceryList.removeChild(itemContain);
+    if (userInput.value === "") {
+        userInput.style.outline = '2px solid red';
+        addItemButton.style.borderLeft = '3px solid red'
+        window.alert("Please enter item into the input.")
+    } else {
+        itemContain.classList.add('item-container');
+        deleteButton.classList.add('delete-button');
+        checkButton.classList.add('check-button');
+        item.innerText = userInput.value;
+        checkButton.innerText = '-'
+        deleteButton.innerText = 'X';
+        groceryList.appendChild(itemContain);
+        itemContain.appendChild(item);
+        itemContain.appendChild(buttonDiv);
+        buttonDiv.appendChild(checkButton);
+        buttonDiv.appendChild(deleteButton);
+        userInput.value = "";
+            
+        deleteButton.onclick = function() {
+            groceryList.removeChild(itemContain);
+        }
+            
+        checkButton.onclick = function() {
+            item.classList.toggle('line');
+        }
+        
+        userInput.style.outline = '';
+        addItemButton.style.borderLeft = ''
     }
 }
